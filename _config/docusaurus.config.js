a2ddc8da156539edbdbdcd6ea7b922809eb99e3e
aaron <aaron@fmad.io>
2023-01-12 13:20:55 +0000

    exlcude master dir

--- _config/docusaurus.config.js
+++ _config/docusaurus.config.js
@@ -41,6 +41,7 @@ const config = {
           // Remove this to remove the "edit this page" links.
           //editUrl:
           //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          exclude: ['**/master/**']
         },
 /*
         blog: {
