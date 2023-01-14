03c34e7b65379ef1bf8b137dab106e5377e4862f
aaron <aaron@fmad.io>
2023-01-14 09:39:30 +0000

    expand generation function

--- _config/generate.lua
+++ _config/generate.lua
@@ -3,18 +3,54 @@
 print("md file generator")
 
 -----------------------------------------------------------------------------------
+
+function string:split( inSplitPattern, outResults )
+
+    if not outResults then
+        outResults = { }
+    end
+    local theStart = 1
+    local theSplitStart, theSplitEnd = string.find( self, inSplitPattern, theStart )
+    while theSplitStart do
+        table.insert( outResults, string.sub( self, theStart, theSplitStart-1 ) )
+        theStart = theSplitEnd + 1
+        theSplitStart, theSplitEnd = string.find( self, inSplitPattern, theStart )
+    end
+
+    table.insert( outResults, string.sub( self, theStart ) )
+    return outResults
+end
+
+function string:strip()
+    return self:gsub("^%s*(.-)%s*$", "%1")
+end
+
+function table.count(t)
+
+    local c = 0
+    for a,b in pairs(t) do c = c + 1 end
+    return c
+end
+
+-----------------------------------------------------------------------------------
 -- mapping to nice names
 local TitleMap = {}
-TitleMap["network-config-cli"] = "Network Config (CLI)"
-TitleMap["network-config-gui"] = "Network Config (GUI)"
+TitleMap["network-config-cli"] 		= "Network Config (CLI)"
+TitleMap["network-config-gui"] 		= "Network Config (GUI)"
+TitleMap["scratch-disk-ext4"] 		= "Scratch Disk EXT4"
+TitleMap["management-interface"] 	= "Management Interface"
+TitleMap["capture-port-speed"] 		= "Capture Port Speed"
 
 setmetatable(TitleMap, { __index = function(t, k) return k end } )
 
 
 local SidebarPos = {}
 SidebarPos["Config"] = {}
-SidebarPos["Config"]["network-config-gui"] = 100
-SidebarPos["Config"]["network-config-cli"] = 101
+SidebarPos["Config"]["network-config-gui"] 		= 100
+SidebarPos["Config"]["network-config-cli"] 		= 101
+SidebarPos["Config"]["management-interface"] 	= 102
+
+SidebarPos["Config"]["scratch-disk-ext4"] 		= 150
 
 SidebarPos["Settings"] 				= {} 
 SidebarPos["Settings"]["capture"] 	= 200
@@ -52,7 +88,6 @@ local MasterGenerate = function(SKU, Source, Section, Name)
 		write("sidebar_position: %i\n", Pos)
 	end
 	write("---\n")
-print(Section, Name, Pos)
 
 	-- header tags
 	local SKUStr = SKU
@@ -65,8 +100,46 @@ print(Section, Name, Pos)
 	write("\n")	
 	write("\n")	
 
+	local LineEnable = {} 
+
 	for l in In:lines() do
-		Out:write(l.."\n")
+
+		local o = l
+		if (l:sub(1, 3) == "-- ") then
+
+			-- dont include pre processor
+			o = nil
+
+			local cmd = l:sub(4, -1)
+			local s = cmd:split("[%s]+")
+			local sku  = s[1] 
+			local verb = s[2] 
+			print(sku, verb)
+
+
+			if (verb == "begin") then
+				LineEnable[sku] = true	
+			end
+
+			if (verb == "end") then
+				LineEnable[sku] = nil 
+			end
+
+
+		-- not metacommand check if its valid
+		else
+
+			if (table.count(LineEnable) > 0) then
+				
+				if (LineEnable[SKU] == nil) then
+					o = nil
+				end
+			end
+		end
+
+		if (o ~= nil) then
+			Out:write(o.."\n")
+		end
 	end
 
 	In:close()
@@ -75,17 +148,22 @@ end
 
 -----------------------------------------------------------------------------------
 
-MasterGenerate("fmadio20v3", "../master/Config/network-configuration-gui.md", "Config", "network-config-gui")
-MasterGenerate("fmadio20v3", "../master/Config/network-configuration-cli.md", "Config", "network-config-cli")
+MasterGenerate("fmadio20v3", "../master/Config/network-configuration-gui.md", 		"Config", "network-config-gui")
+MasterGenerate("fmadio20v3", "../master/Config/network-configuration-cli.md", 		"Config", "network-config-cli")
+MasterGenerate("fmadio20v3", "../master/Config/scratch-disk-ext4.md", 				"Config", "scratch-disk-ext4")
+MasterGenerate("fmadio20v3", "../master/Config/management-interface.md", 			"Config", "management-interface")
+MasterGenerate("fmadio20v3", "../master/Config/capture-port-link-speed-f100.md",	"Config", "capture-port-speed")
 
-MasterGenerate("fmadio20p3", "../master/Config/network-configuration-gui.md", "Config", "network-config-gui")
-MasterGenerate("fmadio20p3", "../master/Config/network-configuration-cli.md", "Config", "network-config-cli")
 
-MasterGenerate("fmadio40v3", "../master/Config/network-configuration-gui.md", "Config", "network-config-gui")
-MasterGenerate("fmadio40v3", "../master/Config/network-configuration-cli.md", "Config", "network-config-cli")
+MasterGenerate("fmadio20p3", "../master/Config/network-configuration-gui.md", 		"Config", "network-config-gui")
+MasterGenerate("fmadio20p3", "../master/Config/network-configuration-cli.md", 		"Config", "network-config-cli")
 
-MasterGenerate("fmadio100v2", "../master/Config/network-configuration-gui.md", "Config", "network-config-gui")
-MasterGenerate("fmadio100v2", "../master/Config/network-configuration-cli.md", "Config", "network-config-cli")
+MasterGenerate("fmadio40v3", "../master/Config/network-configuration-gui.md", 		"Config", "network-config-gui")
+MasterGenerate("fmadio40v3", "../master/Config/network-configuration-cli.md", 		"Config", "network-config-cli")
 
-MasterGenerate("fmadio100p3", "../master/Config/network-configuration-gui.md", "Config", "network-config-gui")
-MasterGenerate("fmadio100p3", "../master/Config/network-configuration-cli.md", "Config", "network-config-cli")
+MasterGenerate("fmadio100v2", "../master/Config/network-configuration-gui.md", 		"Config", "network-config-gui")
+MasterGenerate("fmadio100v2", "../master/Config/network-configuration-cli.md", 		"Config", "network-config-cli")
+MasterGenerate("fmadio100v2", "../master/Config/capture-port-link-speed-f100.md",	"Config", "capture-port-speed")
+
+MasterGenerate("fmadio100p3", "../master/Config/network-configuration-gui.md", 		"Config", "network-config-gui")
+MasterGenerate("fmadio100p3", "../master/Config/network-configuration-cli.md", 		"Config", "network-config-cli")
